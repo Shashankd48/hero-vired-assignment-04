c102e9038d651674c2443ff1a3043d494b26755f
Shashank Dubey <jfriday464@gmail.com>
2026-06-28 15:04:03 +0530

    Add healthchecks for the backend, frontend and compose

--- code/backend/index.js
+++ code/backend/index.js
@@ -12,10 +12,28 @@ const tripRoutes = require('./routes/trip.routes')
 
 app.use('/trip', tripRoutes) // http://localhost:3001/trip --> POST/GET/GET by ID
 
-app.get('/hello', (req,res)=>{
+app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(PORT, ()=>{
+app.get("/health", (req, res) => {
+
+    if (mongoose.connection.readyState === 1) {
+
+        return res.status(200).json({
+            status: "UP",
+            database: "CONNECTED"
+        });
+
+    }
+
+    return res.status(503).json({
+        status: "DOWN",
+        database: "DISCONNECTED"
+    });
+
+});
+
+app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`)
 })
