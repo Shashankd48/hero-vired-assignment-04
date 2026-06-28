71e6eb84be260585b6b3731d892e288e5f536c48
Shashank Dubey <jfriday464@gmail.com>
2026-06-28 15:25:38 +0530

    Fixe sin docker build and nginx config

--- code/frontend/src/url.js
+++ code/frontend/src/url.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
+export const baseUrl = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_URL : process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
